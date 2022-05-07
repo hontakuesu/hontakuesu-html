de1b5f3f83efd6c2ea9a57cd9b64bdafc81d89fc
hontakuesu <99927209+hontakuesu@users.noreply.github.com>
2022-05-08 07:15:13 +0800

    Add files via upload

---
+++ DMM ADS 200x720.js
@@ -0,0 +1,13 @@
+Banner = new Array(3);
+
+Banner[0] = "<a href='https://overlord-game.com/dmg_temp'title='Overlord Game'><img src='https://p.dmm.com/p/general/netgame/top/head2/bnr_mftd_ac1u47.jpg' border='0'  height='200px' width='720px'></a>";
+Banner[1] = "<a href='https://dmg.priconne-redive.jp/'title='Princess Connect! Re:Dive Game'><img src='https://p.dmm.com/p/general/netgame/top/head2/bnr_priconner_RTitZV.jpg' border='0'  height='200px' width='720px'></a>";
+Banner[2] = "<a href='https://games.dmm.com/en/detail/tohken/'title='Tohken Online'><img src='https://p.dmm.com/p/general/netgame/top/head2/bnr_825012_MYrlFL.jpg' border='0'  height='200px' width='720px'></a>";
+Banner[3] = "<a href='https://games.dmm.com/en/detail/windboys/'title='Wind Boys!'><img src='https://p.dmm.com/p/general/netgame/top/head2/bnr_646681_1bNZ8M.jpg' border='0'  height='200px' width='720px'></a>";
+Banner[4] = "<a href='https://games.dmm.com/en/detail/godkiller_aria/'title='Godkiller Aria'><img src='https://p.dmm.com/p/general/netgame/top/head2/bnr_623905_ociFz0.jpg' border='0'  height='200px' width='720px'></a>";
+Banner[5] = "<a href='https://geass-game.com/'title='Geass Game'><img src='https://p.dmm.com/p/general/netgame/top/head2/bnr_codegeass_tPGoJR.jpg' border='0'  height='200px' width='720px'></a>";
+Banner[6] = "<a href='https://sangoku.15combo.com/'title='Three Kingdoms Blade'><img src='https://p.dmm.com/p/general/netgame/top/head2/bnr_38691165_IIQnKT.jpg' border='0'  height='200px' width='720px'></a>";
+Banner[7] = "<a href='https://www.dmm.com/netgame/feature/crimson_youmataisen.html'title='Crimson Youma Taisen'><img src='https://p.dmm.com/p/general/netgame/top/head2/bnr_626544_wU8vWj.jpg' border='0'  height='200px' width='720px'></a>";
+
+index = Math.floor(Math.random() * Banner.length);
+document.write(Banner[index]);
